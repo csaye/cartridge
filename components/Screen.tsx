9ac48e0f8327400cb476eb4543f620e783cd5783
Cooper Saye <coopersaye@gmail.com>
2022-11-12 11:49:03 -0500

    Listen for screen events.

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -13,12 +13,32 @@ export default function Screen() {
     ctx = canvas?.getContext('2d');
   }, []);
 
+  // called on mouse down
+  function onMouseDown() {
+  }
+
+  // called on mouse move
+  function onMouseMove() {
+  }
+
+  // called on mouse up
+  function onMouseUp() {
+  }
+
+  // called on mouse leave
+  function onMouseLeave() {
+  }
+
   return (
     <div className={styles.container}>
       <canvas
         ref={canvasRef}
         width="256"
         height="256"
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+        onMouseLeave={onMouseLeave}
       />
     </div>
   );
