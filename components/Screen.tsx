d43ceb864e6211d1d8374e80330ea06a091b2373
Cooper Saye <coopersaye@gmail.com>
2022-11-11 23:56:03 -0500

    Get screen canvas context.

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useRef } from 'react';
 import styles from '../styles/components/Screen.module.scss';
 
+let canvas;
+let ctx;
+
 export default function Screen() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  // get canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas?.getContext('2d');
+  }, []);
+
   return (
     <div className={styles.container}>
       <canvas
+        ref={canvasRef}
         width="256"
         height="256"
       />
