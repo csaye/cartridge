2ef612861fd7f53f0f77746ca3b118f109c6ef56
Cooper Saye <coopersaye@gmail.com>
2022-11-11 23:45:53 -0500

    Construct screen canvas.

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -3,6 +3,10 @@ import styles from '../styles/components/Screen.module.scss';
 export default function Screen() {
   return (
     <div className={styles.container}>
+      <canvas
+        width="256"
+        height="256"
+      />
     </div>
   );
 }
