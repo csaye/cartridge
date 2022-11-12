13e293c654dd32d3ab08d3a13156fb6582933b2d
Cooper Saye <coopersaye@gmail.com>
2022-11-11 23:46:19 -0500

    Update index page layout.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Screen from '../components/Screen';
 import styles from '../styles/pages/Index.module.scss';
 
 export default function Index() {
   return (
     <div className={styles.container}>
+      <div className={styles.content}>
+        <Screen />
+      </div>
     </div>
   );
 }
