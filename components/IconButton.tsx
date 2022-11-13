0b0aebe3251b76785cbd23e40e1a3e77fd0c0a04
Cooper Saye <coopersaye@gmail.com>
2022-11-13 14:25:59 -0500

    Update icon button props.

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,6 +1,14 @@
+import Image from 'next/image';
 import styles from '../styles/components/IconButton.module.scss';
 
-export default function IconButton() {
+type Props = {
+  onClick: () => void;
+  icon: string;
+};
+
+export default function IconButton(props: Props) {
+  const { onClick, icon } = props;
+
   return (
     <div className={styles.container}>
     </div>
