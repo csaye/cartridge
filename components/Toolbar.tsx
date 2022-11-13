c08afc5aeb3d359d6bede4784096439d46c8de2a
Cooper Saye <coopersaye@gmail.com>
2022-11-12 21:05:00 -0500

    Create play button.

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
 import styles from '../styles/components/Toolbar.module.scss';
 
 export default function Toolbar() {
+  const [playing, setPlaying] = useState(false);
+
   return (
     <div className={styles.container}>
+      {
+        playing ?
+          <button onClick={() => setPlaying(false)}>
+            Stop
+          </button> :
+          <button onClick={() => setPlaying(true)}>
+            Play
+          </button>
+      }
     </div>
   );
 }
