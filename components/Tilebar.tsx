e90ae06e8ea01da10216e83f4087c9ebf0bb54fe
Cooper Saye <coopersaye@gmail.com>
2022-11-12 23:56:21 -0500

    Update tilebar props.

--- components/Tilebar.tsx
+++ components/Tilebar.tsx
@@ -1,6 +1,22 @@
+import { Dispatch, MouseEvent, useEffect, useRef, useState } from 'react';
 import styles from '../styles/components/Tilebar.module.scss';
+import getCanvasContext from '../util/getCanvasContext';
+import { getMouseIndex } from '../util/mouse';
+import { screenPixels, screenTiles, tilePixels } from '../util/units';
 
-export default function Tilebar() {
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
+let tilesImage: HTMLImageElement;
+let container: HTMLDivElement;
+
+type Props = {
+  selectedIndex: number;
+  setSelectedIndex: Dispatch<number>;
+};
+
+export default function Tilebar(props: Props) {
+  const { selectedIndex, setSelectedIndex } = props;
+
   return (
     <div className={styles.container}>
     </div>
