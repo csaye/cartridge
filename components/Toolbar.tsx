490298b5a2d292021aaeeacf5fb818a636d3301a
Cooper Saye <coopersaye@gmail.com>
2023-01-14 23:58:07 -0500

    Remove toolbar selectors.

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -51,34 +51,7 @@ export default function Toolbar(props: Props) {
           />
       }
       {
-        (!playing && selectedIndex === -1) &&
-        <div className={styles.selectArrow} />
       }
-      <span style={{ flexGrow: 1 }} />
-      <select
-        value={mapWidth}
-        onChange={e => {
-          const newMapWidth = parseInt(e.target.value);
-          if (mapX >= newMapWidth) setMapX(newMapWidth - 1);
-          setMapWidth(newMapWidth);
-        }}
-      >
-        {Array(maxHorizontal).fill(0).map((v, i) =>
-          <option key={i} value={i + 1}>↔ {i + 1}</option>
-        )}
-      </select>
-      <select
-        value={mapHeight}
-        onChange={e => {
-          const newMapHeight = parseInt(e.target.value);
-          if (mapY >= newMapHeight) setMapY(newMapHeight - 1);
-          setMapHeight(newMapHeight);
-        }}
-      >
-        {Array(maxVertical).fill(0).map((v, i) =>
-          <option key={i} value={i + 1}>↕ {i + 1}</option>
-        )}
-      </select>
     </div>
   );
 }
